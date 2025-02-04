43e90e985cbcb0594421ba1f30ebbc45281263e6
victor <oguntayovictor65@gmail.com>
2025-02-04 21:40:11 +0100

    server setup

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('04 Store API')
+const express = require("express");
+// middlewares
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+const app = express();
+
+app.use(express.json());
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Server is listening at port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
