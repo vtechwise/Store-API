67eef9caed63ad3873a4825b5b54c1108576b418
victor <oguntayovictor65@gmail.com>
2025-02-05 17:06:14 +0100

    add pagination logic

--- controllers/products.js
+++ controllers/products.js
@@ -26,9 +26,14 @@ const getAllProduct = async (req, res) => {
     const fieldsList = fields.split(",".join(" "));
     result = result.select(fieldsList);
   }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
-  res.status(200).json({ products, nbHits: products.length });
+  res.status(200).json({nbHits: products.length, products });
 };
 
 const getAllProductsStatic = (req, res) => {
